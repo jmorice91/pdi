953825d218be95f70260f83d4582039cf011eab4
Etienne Ndamlabin <ndamlabin@gmail.com>
2026-07-01 10:56:41 +0200

    Correct size checking config

--- plugins/damaris/tests/test_distinguish_data_and_dataset.c
+++ plugins/damaris/tests/test_distinguish_data_and_dataset.c
@@ -34,17 +34,22 @@
 #define IMX 5
 
 const char* CONFIG_FOR_READING_RESULT
-	= "logging: trace                                                                      \n"
-	  "metadata:                                                                             \n"
-	  "  nn: int                                                                                 \n"
-	  "data:                                                                                     \n"
+	= "logging: trace                                               	\n"
+	  "metadata:                                                     	\n"
+	  "  nn: int                                                    	\n"
+	  "data:                                                       		\n"
 	  "  damaris_values: {size: ['$nn'], type: array, subtype: int}     \n"
-	  "plugins:                                                                                 \n"
-	  "  decl_hdf5:                                                                           \n"
-	  "    - file: './HDF5_files/distinguish_data_and_dataset_It0.h5'\n"
-	  "      read:                                                                               \n"
-	  "        damaris_values:                                                           \n"
-	  "          dataset: written_values_ds                                        \n";
+	  "plugins:                                                       	\n"
+	  "  decl_hdf5:                                                   	\n"
+	  "    - file: './HDF5_files/distinguish_data_and_dataset_It0.h5'	\n"
+	  "      read:                                                    	\n"
+	  "        damaris_values:                                          \n"
+	  "          dataset: written_values_ds                             \n"
+	  "    - file: './HDF5_files/distinguish_data_and_dataset_It0.h5'	\n"
+	  "      on_event: 'read_size'										\n"
+	  "      read:														\n"
+	  "        nn:														\n"
+	  "          size_of: written_values_ds								\n";
 
 int main(int argc, char* argv[])
 {
